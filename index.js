72e9f047cabd8dbee1d3caca6736cd14e122c3a8
robert-codecrew <robert@code-crew.org>
2022-08-22 12:39:44 -0500

    adding

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// !: REQUIRE THE DOTENV MODULE!!!
+
+// TODO: Require the Express module
+// TODO: Require the BODY-PARSER module
+
+// ?: define the port number for the Express server to connect to
+
+// TODO: instantiate the Express module
+
+// TODO: Express will listen on the specified port
+
+
+'use strict';
+// initialize the environment variables with the dotenv package
+require('dotenv').config()
+const cors = require('cors');
+// bring in the Express package
+const express = require('express');
+
+// instantiate the Express application
+const app = express();
+app.use(cors());
+
+// set a port number for the server to listen on
+const port = 3001;
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello from the root')
+})
+
+app.get('/starwars', (req, res) => {
+    res.send({
+        character: 'Luke Skywalker',
+        abilities: ['combat', 'White Magik', 'skating'],
+        curse: 'Damned Father',
+        weakness: "Princess Leia"
+    })
+})
+
+
+// turn the server on to listen on specified port
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
+
+
+
